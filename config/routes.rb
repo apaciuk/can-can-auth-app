7ff94cdf01929186ffd7057403c57b4732711ede
Paul Anthony McGowan <info@xhostcom.com>
2022-07-24 21:59:54 +0100

    Nest routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  resources :questions
+  resources :questions do 
+    resources :answers, only: [:create, :destroy]
+  end
   get '/privacy', to: 'home#privacy'
   get '/terms', to: 'home#terms'
 authenticate :user, lambda { |u| u.admin? } do
