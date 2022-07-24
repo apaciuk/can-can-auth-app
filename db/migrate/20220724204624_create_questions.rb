597647347d7e590c677619da401739cd4c3b42f5
Paul Anthony McGowan <info@xhostcom.com>
2022-07-24 21:52:18 +0100

    References

--- db/migrate/20220724204624_create_questions.rb
+++ db/migrate/20220724204624_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[7.0]
   def change
     create_table :questions do |t|
       t.string :content
-
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
