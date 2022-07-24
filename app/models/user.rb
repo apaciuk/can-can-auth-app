7b71131cfe694c4268451568b96067e7162dc99d
Paul Anthony McGowan <info@xhostcom.com>
2022-07-24 21:49:25 +0100

    Relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable
-
+  has_many :questions, dependent: :destroy
   has_one_attached :avatar
   has_person_name
   has_noticed_notifications
